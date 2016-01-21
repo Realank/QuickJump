51e923b633fb62f1e3c44834190b65d9eca8f90c
Realank <realank@126.com>
2016-01-21 21:50:34 +0800

    add project

---
+++ QuickJump/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  QuickJump
+//
+//  Created by Realank-Mac on 16/1/21.
+//  Copyright © 2016年 Realank-Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let appDict = ["weixin":"微信","instagram":"Instagram","mqq":"QQ","taobao":"淘宝","alipay":"支付宝","sinaweibo":"微博","youku":"优酷","imeituan":"美团","wccbyihaodian":"一号店","yddictproapp":"有道词典","zhihu":"知乎","dianping":"大众点评","doubanradio":"豆瓣FM","http":"Safari","ibooks":"iBooks"]
+    var installedAppList : [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let canOpenApp = UIApplication.sharedApplication().canOpenURL(NSURL(string: "weixin://")!)
+//        print(canOpenApp)
+//        UIApplication.sharedApplication().openURL(NSURL(string: "weixin://")!)
+        
+        for appName in appDict.keys {
+            let url = NSURL(string: "\(appName)://")
+            if let appUrl = url {
+                if UIApplication.sharedApplication().canOpenURL(appUrl) {
+                    installedAppList .append(appName)
+                }
+            }
+        }
+        
+        print(installedAppList)
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "sinaweibo://")!)
+    }
+
+}
+
